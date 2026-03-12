a21be020a16ce2bd9b350e6ced1af59aac1fb86b
imranrizvi648 <de.imran@brbgroup.pk>
2026-03-12 11:23:52 +0500

    added multi-page build support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path' // Ye line zaroori hai
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html')
+      }
+    }
   }
 })
